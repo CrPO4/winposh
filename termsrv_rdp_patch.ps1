7a6e154387b2786bb1e9f966e021b9aee2bba038
CrPO4 <32630390+CrPO4@users.noreply.github.com>
2024-08-25 14:42:07 +0300

    2024-08-25
    Added support for both "classic" Powershell and "modern" Powershell.
    Fixed "hyphen" issue (https://github.com/maxbakhub/winposh/pull/1)
    Fixed "services left down" issue (https://github.com/maxbakhub/winposh/issues/8)
    Fixed "backup" issue (running script again would overwrite your backup previously, now it doesn't)
    Fixed "termsrv.patched.dll" being left in directory (now properly removed when is not required anymore)

--- termsrv_rdp_patch.ps1
+++ termsrv_rdp_patch.ps1
@@ -1,17 +1,31 @@
 # PowerShell script used to patch termsrv.dll file and allow multiple RDP connections on Windows 10 (1809 and never) and Windows 11 
 # Details here http://woshub.com/how-to-allow-multiple-rdp-sessions-in-windows-10/
+# Stop RDP service, make a backup of the termsrv.dllfile and change the permissions
 
+$newByteSyntax = [System.Version]$(Get-Host | Select-Object "Version" -ExpandProperty "Version") -ge [System.Version]"6.0.0"
+$backupExists = Test-Path -Path c:\windows\system32\termsrv.dll.backup
 
-# Stop RDP service, make a backup of the termsrv.dllfile and change the permissions 
 Stop-Service UmRdpService -Force
 Stop-Service TermService -Force
 $termsrv_dll_acl = Get-Acl c:\windows\system32\termsrv.dll
-Copy-Item c:\windows\system32\termsrv.dll c:\windows\system32\termsrv.dll.copy
+
+if (-Not $backupExists)
+{
+    Copy-Item c:\windows\system32\termsrv.dll c:\windows\system32\termsrv.dll.backup
+}
+
 takeown /f c:\windows\system32\termsrv.dll
 $new_termsrv_dll_owner = (Get-Acl c:\windows\system32\termsrv.dll).owner
-cmd /c "icacls c:\windows\system32\termsrv.dll /Grant $($new_termsrv_dll_owner):F /C"
+cmd /c "icacls c:\windows\system32\termsrv.dll /Grant ""$($new_termsrv_dll_owner):F"" /C"
 # search for a pattern in termsrv.dll file 
-$dll_as_bytes = Get-Content c:\windows\system32\termsrv.dll -Raw -Encoding byte
+$dll_as_bytes = ''
+if ($newByteSyntax) 
+{ 
+	$dll_as_bytes = Get-Content c:\windows\system32\termsrv.dll -Raw -AsByteStream
+} else 
+{ 
+	$dll_as_bytes = Get-Content c:\windows\system32\termsrv.dll -Raw -Encoding Byte
+}
 $dll_as_text = $dll_as_bytes.forEach('ToString', 'X2') -join ' '
 $patternregex = ([regex]'39 81 3C 06 00 00(\s\S\S){6}')
 $patch = 'B8 00 01 00 00 89 81 38 06 00 00 90'
@@ -21,6 +35,8 @@ If ($checkPattern -ne $null) {
 }
 Elseif (Select-String -Pattern $patch -InputObject $dll_as_text) {
     Write-Output 'The termsrv.dll file is already patched, exiting'
+    Start-Service UmRdpService
+    Start-Service TermService
     Exit
 }
 else { 
@@ -28,11 +44,18 @@ else {
 }
 # patching termsrv.dll
 [byte[]] $dll_as_bytes_replaced = -split $dll_as_text_replaced -replace '^', '0x'
-Set-Content c:\windows\system32\termsrv.dll.patched -Encoding Byte -Value $dll_as_bytes_replaced
+if ($newByteSyntax) 
+{ 
+	Set-Content c:\windows\system32\termsrv.dll.patched -AsByteStream -Value $dll_as_bytes_replaced
+} else 
+{ 
+	Set-Content c:\windows\system32\termsrv.dll.patched -Encoding Byte -Value $dll_as_bytes_replaced
+}
 # comparing two files 
 fc.exe /b c:\windows\system32\termsrv.dll.patched c:\windows\system32\termsrv.dll
 # replacing the original termsrv.dll file 
 Copy-Item c:\windows\system32\termsrv.dll.patched c:\windows\system32\termsrv.dll -Force
+Remove-Item c:\windows\system32\termsrv.dll.patched -Force
 Set-Acl c:\windows\system32\termsrv.dll $termsrv_dll_acl
 Start-Service UmRdpService
 Start-Service TermService
